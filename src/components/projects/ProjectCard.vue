7bfb8c06dd4189dff44707e445def00ee14f6396
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-02 17:40:11 +0200

    added abstract and card content

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -1,12 +1,29 @@
 <script>
 export default {
     name: 'ProjectCard',
-    props: { project: Object }
+    props: { project: Object },
+    computed: {
+        abstract() {
+            const abstract = this.project.content.slice(0, 350);
+            return abstract + '...';
+        }
+    }
 };
 </script>
 
 <template>
-    <div class="card"></div>
+    <div class="card mt-3 mb-5">
+        <div class="card-header">
+            <h2>{{ project.title }}</h2>
+        </div>
+        <div class="card-body">
+            <p>{{ abstract }}</p>
+        </div>
+        <div class="card-footer d-flex justify-content-between">
+            <address>By: {{ project.user_id }}</address>
+            <small>Pubblicato il {{ project.created_at }}</small>
+        </div>
+    </div>
 </template>
 
 
