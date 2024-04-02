43505b87cf9e55b1cff2dc8b53d22f21aa29a68a
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-02 17:15:28 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'Boolfolio',
+  components: { AppHeader }
+};
+</script>
+
+<template>
+  <AppHeader />
+  <main class="container py-4">
+    <h1>Boolfolio</h1>
+  </main>
+</template>
+
+<style></style>
