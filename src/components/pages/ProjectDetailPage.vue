e08f5503b750cbfb5b4b8ff2a120915461c21097
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-03 17:37:57 +0200

    added projectdetailpage

--- src/components/pages/ProjectDetailPage.vue
+++ src/components/pages/ProjectDetailPage.vue
@@ -1,11 +1,41 @@
 <script>
+import ProjectCard from '../projects/ProjectCard.vue';
+import AppLoader from '../AppLoader.vue';
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects/';
+
 export default {
     name: 'ProjectDetailPage',
+    components: { ProjectCard, AppLoader },
+    data: () => ({
+        project: null,
+        isLoading: false
+    }),
+    methods: {
+        getProject() {
+            this.isLoading = true;
+            axios.get(endpoint + this.$route.params.id)
+                .then(res => {
+                    this.project = res.data;
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+                .then(() => {
+                    this.isLoading = false;
+                })
+        }
+    },
+    created() {
+        this.getProject();
+    }
 }
 </script>
 
 <template>
-    <h1>Dettaglio progetto</h1>
+    <AppLoader v-if="isLoading && !project" />
+    <ProjectCard v-if="!isLoading && project" :project="project" />
+
 </template>
 
 <style></style>
