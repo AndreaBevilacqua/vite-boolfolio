2d270bad092bcb20c7af1ae293df7829f5138058
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-03 16:57:43 +0200

    fixed app page

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,19 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import ProjectsList from './components/projects/ProjectsList.vue';
-import axios from 'axios';
-const endpoint = 'http://localhost:8000/api/projects/';
+
 
 export default {
   name: 'Boolfolio',
-  components: { AppHeader, ProjectsList },
-  data: () => ({ projects: [], isLoading: false }),
-  methods: {
-    fetchProjects() {
-      this.isLoading = true;
-      axios.get(endpoint).then(res => {
-        this.projects = res.data;
-      }).catch(err => {
-        console.error(err);
-      }).then(() => {
-        this.isLoading = false;
-      })
-    }
-  },
-  created() {
-    this.fetchProjects();
-  }
+  components: { AppHeader, HomePage, ContancUsPage },
+
 };
 </script>
 
 <template>
   <AppHeader />
   <main class="container py-4">
-    <h1>Boolfolio</h1>
-    <AppLoader v-if="isLoading" />
-    <ProjectsList v-else :projects="projects" />
-  </main>
 
+  </main>
 </template>
 
 <style></style>
