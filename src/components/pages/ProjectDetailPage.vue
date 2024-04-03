d92bbf6b89615f425a456bdaeafff578f4d293e0
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-03 17:25:09 +0200

    created project detail

---
+++ src/components/pages/ProjectDetailPage.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'ProjectDetailPage',
+}
+</script>
+
+<template>
+    <h1>Dettaglio progetto</h1>
+</template>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from '../components/pages/HomePage.vue';
 import ContactUsPage from '../components/pages/ContactUsPage.vue';
+import ProjectDetailPage from '../components/pages/ProjectDetailPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/", name: "home", component: HomePage },
+        { path: "/projects/:id", name: "project-detail", component: ProjectDetailPage },
         { path: "/contact-us", name: "contact-us", component: ContactUsPage },
         { path: "/:pathMatch(.*)*", redirect: "/" }
     ]
