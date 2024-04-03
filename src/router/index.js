76f1ca3692d3e0ce8c06a4c2aaebf5f2969bce42
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-03 17:04:22 +0200

    imported router

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from "../components/pages/HomePage.vue";
+import ContactUsPage from "../components/pages/ContactUsPage.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/", name: "home", component: HomePage },
+        { path: "/contact-us", name: "contact-us", component: ContactUsPage }
+    ]
+});
+
+export { router };
